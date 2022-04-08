9292bec75db09d6bcac70b64d942e9e917646595
Wayne Graham <wgraham@clir.org>
2022-04-08 10:13:22 -0400

    Refactor Pages controller
    - Statically calls the home view
    - Updates routes
    - Adds Brakeman testing to CI
    
    - closes #58

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,12 @@
 ##
 # Page controller
 class PagesController < ApplicationController
-  def show
-    if valid_page?
-      render template: "pages/#{params[:page]}"
-    else
-      render file: 'public/404.html', status: :not_found
-    end
+
+  def home
   end
 
   def stats
     @proposals = Proposal.all.order(:title)
   end
 
-  private
-
-  def valid_page?
-    File.exist?(
-      Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb")
-    )
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
   end
 
   get 'stats', to: 'proposals#stats'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'proposals#index'
-  # root 'pages#show', page: 'home'
+  # root 'pages#home'
 
-  get '/pages/*page' => 'pages#show'
 end
