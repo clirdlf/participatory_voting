3bbdd493d7bf98d011867ff72a1fcc0fa13bca56
Wayne Graham <wgraham@clir.org>
2022-04-18 10:00:18 -0400

    Add Forum 2022 Contribution Formats
    Adds additional contribution formats used for Forum 2022.
    
    closes #55

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# require 'faker'
+require 'faker'
 
-# contribution_types = [
-#   '2021 DLF Forum',
-#   'Digital Preservation 2021',
-#   'Learn@DLF'
-# ]
+contribution_types = [
+  'Learn@DLF',
+  '2022 DLF Forum',
+  'Digital Preservation 2022: Preserving Legacy',
+  'Digitizing Hidden Collections Symposium'
+]
 
-# presentation_types = [
-#   '55-minute Panel',
-#   '18-minute Presentation',
-#   '18-Minute Talks/Demos',
-#   '7-Minute Snapshot (7x7)',
-#   'Breakfast/Lunch Working Session'
-# ]
+presentation_types = [
+  'Three-hour Workshop',
+  '45-minute Panel',
+  '45-minute Workshop',
+  '45-minute Working Session',
+  '15-minute Presentation',
+  '5-minute Lightning Talk',
+  '45-minute Working Group Meeting',
+  '15-minute Talk/Demo',
+  '2-minute Lightning Talk/Poster',
+  '45-minute DIY Session',
+  '15-minute Paper',
+  'Poster',
+  '5-minute Demonstration Video'
+]
 
-# 10.times do |_p|
-#   Proposal.create(
-#     author: Faker::Name.name,
-#     title: Faker::Book.title,
-#     abstract: Faker::Hipster.paragraph(sentence_count: 5),
-#     contribution_type: contribution_types.sample,
-#     contribution_format: presentation_types.sample
-#   )
-# end
+13.times do |p|
+  Proposal.create(
+    author: Faker::Name.name,
+    title: Faker::Book.title,
+    abstract: Faker::Hipster.paragraph(sentence_count: 5),
+    contribution_type: contribution_types.sample,
+    contribution_format: presentation_types[p]
+  )
+end
 
 # t.string "author"
 # t.string "title"
