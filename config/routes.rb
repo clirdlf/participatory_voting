a3bf3820899326a273230e0d7b79ca2fc53809b8
Wayne Graham <wgraham@clir.org>
2022-06-01 08:40:21 -0400

    Turn off voting

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'proposals#index'
-  # root 'pages#home'
+  # root 'proposals#index'
+  root 'pages#home'
 end
